c32cad62902f4b3c334accf4dec6b70ecf2af3ae
Priyanka <priyanka3007@gmail.com>
2017-06-13 16:05:34 -0700

    Writing regex for a phone number

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ target.innerHTML =
 string.replace(regex, string=>`<span>${string}</span>`);
 }
 
-var string= "Is this This his"
+var string= `800-545-0990
+(555) 777-9000
+4564567456`
 // var regex = new RegExp("it")
-var regex = /is/gi
+var regex = /\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}/g
+
+console.log(string.replace(regex, 'area code: $1'))
 
 output(string, regex, document.querySelector('pre'))
 // console.log(regex.exec(string));
