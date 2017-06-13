511b7e84dd46ba694e7e3db60ba05780252fc57a
Priyanka <priyanka3007@gmail.com>
2017-06-13 16:08:45 -0700

    Positive/negative lookahead

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ target.innerHTML =
 string.replace(regex, string=>`<span>${string}</span>`);
 }
 
-var string= `800-545-0990
-(555) 777-9000
-4564567456`
+var string= `foo
+foobar
+foobaz
+fooboo`
 // var regex = new RegExp("it")
-var regex = /\(?(\d{3})\)?[\s-]?\d{3}[\s-]?\d{4}/g
+var regex = /foo(?!bar|boo)/g
 
 console.log(string.replace(regex, 'area code: $1'))
 
